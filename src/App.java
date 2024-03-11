d055cdd456e5075497090fb0f44b9ba6a6a257ef
Teddy <teddyfriedman27@gmail.com>
2024-03-11 13:46:29 -0400

    Please Help!

--- src/App.java
+++ src/App.java
@@ -9,20 +9,29 @@ public class App extends PApplet {
     float line2Y = 190;
     float line2X = 300;
     float line3Y = 275;
-    float line3X = 000;
+    float line3X = 50;
     float line4Y = 400;
     float line4X = 400;
     float line5Y = 475;
     float line5X = 250;
     float line6Y = 550;
-    float line6X = 000;
+    float line6X = 100;
     float line7X = 0;
     float line7Y = 1;
+    float line8X = 0;
+    float line8Y = 600;
     float lineWidth = 400;
     float lineWidth7 = 800;
+    float lineWidth8 = 800;
+    int startTime;
+    int elapsedTime;
     boolean leftpressed = false;
     boolean rightpressed = false;
-    int speed = -1;
+    double speed = -1;
+    double speedUp1 = -2;
+    double speedUp2 = -3;
+    double speedUp3 = -4;
+    double ballSpeed = -5;
     int gravity = -1;
     boolean ballTouchingLine = false;
     boolean running = true;
@@ -35,9 +44,9 @@ public class App extends PApplet {
         size(800, 600);
 
     }
-   
+
     public boolean isBallTouchingLine(float lineY, float lineX) {
-        if (ballY >= lineY - 26 && ballY <= lineY + 26 && ballX >= lineX && ballX <= lineX + lineWidth) {
+        if (ballY >= lineY - 26 && ballY <= lineY + 6 && ballX >= lineX && ballX <= lineX + lineWidth) {
             System.out.println("Ball touching line");
             return true;
 
@@ -51,19 +60,30 @@ public class App extends PApplet {
         ball = 45;
         ballX = width / 2;
         ballY = height / 2;
+        startTime = millis(); 
+        line1Y = random(lineWidth);
+        line2Y = random(lineWidth);
+        line3Y = random(lineWidth);
+        line4Y = random(lineWidth);
+        line5Y = random(lineWidth);
+        line6Y = random(lineWidth);
+
     }
 
     public void draw() {
-        if(running){
+        if (running) {
             line1Y += speed;
-        line2Y += speed;
-        line3Y += speed;
-        line4Y += speed;
-        line5Y += speed;
-        line6Y += speed;
+            line2Y += speed;
+            line3Y += speed;
+            line4Y += speed;
+            line5Y += speed;
+            line6Y += speed;
+            elapsedTime = (millis() - startTime) / 1000;
+            speedUp1();  
+            speedUp2();  
+            speedUp3();  
         }
 
-        
         background(255);
         drawBall();
         drawLine1();
@@ -73,10 +93,17 @@ public class App extends PApplet {
         drawLine5();
         drawLine6();
         drawEndLine();
-        if (isBallTouchingLine(line7X, line7Y)) {
+        drawWinLine();
+
+         if (isBallTouchingLine(line7X, line7Y)) {
             ballTouchingLine = true;
             youLost();
         }
+
+        // if (isBallTouchingLine(line8X, line8Y)) {
+        //     ballTouchingLine = true;
+        //     youWin();
+        // }
 
         if (ballTouchingLine) {
             // Do not update the ball's position if it's touching the line
@@ -89,14 +116,12 @@ public class App extends PApplet {
                 isBallTouchingLine(line3Y, line3X) || isBallTouchingLine(line4Y, line4X)
                 || isBallTouchingLine(line5Y, line5X) || isBallTouchingLine(line6Y, line6X)) {
             ballTouchingLine = true;
-            // ballY = line2Y;
         } else {
             ballTouchingLine = false;
 
             if (ballTouchingLine) {
-                // Do not update the ball's position if it's touching the line
+
             } else {
-                // Fall due to gravity
                 ballY -= gravity;
             }
 
@@ -119,14 +144,24 @@ public class App extends PApplet {
         if (line6Y <= 0) {
             line6Y = height;
         }
+        if (running) {elapsedTime = (millis() - startTime) / 1000;
+
+        // Display elapsed time
+        textSize(16);
+        textAlign(LEFT, TOP);
+        fill(0);
+        text("Elapsed Time: " + elapsedTime + " seconds", 10, 10);
+        }
+
+    
     }
 
     public void drawBall() {
-        if (leftpressed = true) {
-            ballX -= 10;
+        if (leftpressed == true) {
+            ballX += ballSpeed;
         }
-        if (rightpressed = true) {
-            ballX += 10;
+        if (rightpressed == true) {
+            ballX -= ballSpeed;
         }
         ellipse(ballX, ballY, ball, ball);
         fill(0, 0, 255);
@@ -174,6 +209,12 @@ public class App extends PApplet {
         color(255, 128, 0);
     }
 
+    public void drawWinLine() {
+        line(line8X, line8Y, line8X + lineWidth8, line8Y);
+        strokeWeight(6);
+        color(255, 128, 0);
+    }
+
     public void keyPressed() {
         if (keyCode == LEFT) {
             leftpressed = true;
@@ -185,18 +226,25 @@ public class App extends PApplet {
             // } else if (keyCode == DOWN) {
             // ballY += 10;
         }
-
-        if(key == 'r'){
+        if (key == 'r') {
             restartGame();
-            
 
         }
     }
 
-    public void restartGame(){
+    public void restartGame() {
+        ballX = width / 2;
+        ballY = height / 2;
+        line1Y = 100;
+        line2Y = 190;
+        line3Y = 275;
+        line4Y = 400;
+        line5Y = 475;
+        line6Y = 550;
         running = true;
-        //Reset location of all objects
-    }
+        startTime = millis();
+        setup();
+        }
 
     public void keyReleased() {
         if (keyCode == LEFT) {
@@ -205,21 +253,29 @@ public class App extends PApplet {
         if (keyCode == RIGHT) {
             rightpressed = false;
         }
-
-
-
     }
 
     public void youLost() {
         running = false;
+        startTime = 0;
         fill(0); // Set the fill color to black
         textSize(80); // Set the text size to 32 pixels
-        text("You Lost!", 250, 200);
-        text("Hit 'R' to Restart!", 160, 400);
-
+        text("You Lost!", 240, 150);
+        text("Hit 'R' to Restart!", 170, 300);
+        text("Time: " + elapsedTime, 270,400);
     }
-}
 
-// I need help with:
-// Get it to stop running
-// get my ball to move smoothly again
+    public void speedUp1(){
+        if (elapsedTime >= 10)
+            speed = speedUp1;
+    }
+    public void speedUp2(){
+        if (elapsedTime >= 20)
+            speed = speedUp2;
+    }
+    public void speedUp3(){
+        if (elapsedTime >= 30)
+            speed = speedUp3;
+    }
+
+}
